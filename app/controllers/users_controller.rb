bd80b2682c4952f0da83a866ab0a45adb8da0b4f
@FYoshi0118 <yoshiki.f0118@gmail.com>
2018-04-25 16:22:52 +0900

    profile_imageの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def index
   end
 
   def show
     @book = Book.new
     @books = Book.all
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(user).permit(:name, :user_introduction, :profile_image)
   end
 end
